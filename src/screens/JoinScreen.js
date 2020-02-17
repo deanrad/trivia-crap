deb32f8f1b9a4fd14acd90b9f105686ffc2633c6
Dean Radcliffe <dradcliffe@g2.com>
2020-02-17 16:54:27 -0600

    Add event-based (imperative) toast for auth/login using notyf.

--- src/screens/JoinScreen.js
+++ src/screens/JoinScreen.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { trigger } from 'polyrhythm';
+import { trigger, useListener } from 'polyrhythm';
+import { Notyf } from 'notyf';
+
+import 'notyf/notyf.min.css';
+const notyf = new Notyf();
 
 export const JoinScreen = ({ loggedInUser, authUrl, authStates }) => {
   const [user, setUser] = useState(loggedInUser);
@@ -16,6 +20,10 @@ export const JoinScreen = ({ loggedInUser, authUrl, authStates }) => {
     return () => sub && sub.unsubscribe();
   }, [authStates]);
 
+  useListener('auth/login', (_, { user }) => {
+    const msg = user ? `Logged in as ${user}` : 'You have been logged out.';
+    notyf.success(msg);
+  });
   return user ? (
     <LoggedInJoinScreen username={user} />
   ) : (
